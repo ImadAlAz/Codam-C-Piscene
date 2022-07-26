ccda4a4ae920ade6d011030d897431f8a64e70ef
ImadAlAz <110030120+ImadAlAz@users.noreply.github.com>
2022-07-26 13:12:57 +0200

    Add files via upload

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_print_comb.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ial-azha <ial-azha@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/07/11 18:50:01 by ial-azha      #+#    #+#                 */
+/*   Updated: 2022/07/11 18:57:58 by ial-azha      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_commas(void)
+{
+	ft_putchar(',');
+	ft_putchar(' ');
+}
+
+void	print(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (!(a == '7' && b == '8' && c == '9' ))
+	{
+		ft_commas();
+	}
+}
+
+void	ft_print_comb(void)
+{
+	int	num[3];
+
+	num[0] = '0';
+	while (num[0] <= '9')
+	{
+		num[1] = '0';
+		while (num[1] <= '9')
+		{
+			num[2] = '0';
+			while (num[2] <= '9')
+			{
+				if (num[0] < num[1] && num[1] < num[2])
+				{
+					print(num[0], num[1], num[2]);
+				}
+				num[2]++;
+			}
+			num[1]++;
+		}
+		num[0]++;
+	}
+}
