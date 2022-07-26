054b152adc94ef4636bc35470548a053def4895e
ImadAlAz <110030120+ImadAlAz@users.noreply.github.com>
2022-07-26 13:08:49 +0200

    Add files via upload

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_print_reverse_alphabet.c                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ial-azha <ial-azha@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/07/07 22:48:09 by ial-azha      #+#    #+#                 */
+/*   Updated: 2022/07/10 11:44:58 by ial-azha      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		ft_putchar(c);
+		c--;
+	}
+}
