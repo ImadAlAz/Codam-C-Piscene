c1534b398397808ac7400adc55a0ea49de2a74c0
Imad Al Azhar <110030120+ImadAlAz@users.noreply.github.com>
2022-07-26 14:02:18 +0200

    Add files via upload

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_putnbr.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ial-azha <ial-azha@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/07/25 14:53:37 by ial-azha      #+#    #+#                 */
+/*   Updated: 2022/07/25 14:54:30 by ial-azha      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
